89d0d04c2b0672d3cc9839628892e783cf509106
Valentin Vanelslande <valentinvanelslandeacnl@gmail.com>
2018-02-07 12:46:24 -0600

    clang format control_panel.cpp (using clang-format.me)

--- src/citra_qt/control_panel.cpp
+++ src/citra_qt/control_panel.cpp
@@ -10,39 +10,41 @@
 #include "control_panel.h"
 #include "ui_control_panel.h"
 
-ControlPanel::ControlPanel(QWidget *parent) :
-    QDialog(parent, Qt::WindowTitleHint | Qt::WindowCloseButtonHint | Qt::WindowSystemMenuHint),
-    ui(new Ui::ControlPanel)
-{
-    ui->setupUi(this);
-    ui->adapter_connected->setChecked(Settings::values.p_adapter_connected);
-    ui->battery_charging->setChecked(Settings::values.p_battery_charging);
-    ui->battery_level->setCurrentIndex(Settings::values.p_battery_level - 1);
-    connect(ui->adapter_connected, &QCheckBox::stateChanged, this, &ControlPanel::OnAdapterConnectedChanged);
-    connect(ui->battery_charging, &QCheckBox::stateChanged, this, &ControlPanel::OnBatteryChargingChanged);
-    connect(ui->battery_level, static_cast<void (QComboBox::*)(int)>(&QComboBox::currentIndexChanged), this, &ControlPanel::OnBatteryLevelChanged);
-    setWindowTitle("Control Panel");
-    setFixedSize(size());
+ControlPanel::ControlPanel(QWidget *parent)
+    : QDialog(parent, Qt::WindowTitleHint | Qt::WindowCloseButtonHint |
+                          Qt::WindowSystemMenuHint),
+      ui(new Ui::ControlPanel) {
+  ui->setupUi(this);
+  ui->adapter_connected->setChecked(Settings::values.p_adapter_connected);
+  ui->battery_charging->setChecked(Settings::values.p_battery_charging);
+  ui->battery_level->setCurrentIndex(Settings::values.p_battery_level - 1);
+  connect(ui->adapter_connected, &QCheckBox::stateChanged, this,
+          &ControlPanel::OnAdapterConnectedChanged);
+  connect(ui->battery_charging, &QCheckBox::stateChanged, this,
+          &ControlPanel::OnBatteryChargingChanged);
+  connect(ui->battery_level, static_cast<void (QComboBox::*)(int)>(
+                                 &QComboBox::currentIndexChanged),
+          this, &ControlPanel::OnBatteryLevelChanged);
+  setWindowTitle("Control Panel");
+  setFixedSize(size());
 }
 
-ControlPanel::~ControlPanel() {
-    delete ui;
-}
+ControlPanel::~ControlPanel() { delete ui; }
 
 void ControlPanel::OnAdapterConnectedChanged() {
-    Settings::values.p_adapter_connected = ui->adapter_connected->isChecked();
-    SharedPage::shared_page.battery_state.is_adapter_connected.Assign(
-                static_cast<u8>(Settings::values.p_adapter_connected));
+  Settings::values.p_adapter_connected = ui->adapter_connected->isChecked();
+  SharedPage::shared_page.battery_state.is_adapter_connected.Assign(
+      static_cast<u8>(Settings::values.p_adapter_connected));
 }
 
 void ControlPanel::OnBatteryChargingChanged() {
-    Settings::values.p_battery_charging = ui->battery_charging->isChecked();
-    SharedPage::shared_page.battery_state.is_charging.Assign(
-                static_cast<u8>(Settings::values.p_battery_charging));
+  Settings::values.p_battery_charging = ui->battery_charging->isChecked();
+  SharedPage::shared_page.battery_state.is_charging.Assign(
+      static_cast<u8>(Settings::values.p_battery_charging));
 }
 
 void ControlPanel::OnBatteryLevelChanged() {
-    Settings::values.p_battery_level = ui->battery_level->currentIndex() + 1;
-    SharedPage::shared_page.battery_state.charge_level.Assign(
-                static_cast<u8>(Settings::values.p_battery_level));
+  Settings::values.p_battery_level = ui->battery_level->currentIndex() + 1;
+  SharedPage::shared_page.battery_state.charge_level.Assign(
+      static_cast<u8>(Settings::values.p_battery_level));
 }
